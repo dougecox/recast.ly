79146eaf5163367a83a01847220cfbed47ceaf78
Hack Reactor Student <student@hackreactor.com>
2017-04-13 15:12:32 -0700

    Finished searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,31 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  console.log(options);
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    dataType: 'json',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      type: 'video',
+      videoEmbeddable: true
+    },
+    success: function(data) {
+      console.log(data);
+      callback(data.items);
+      console.log("Success");
+    }
+  });
+  /*var info;
+  let key = options.key;
+  let query = options.query;
+  let max = options.max;
+  $.get( "https://www.googleapis.com/youtube/v3/search", function(data) {
+    info = data;
+    console.log(info);
+  });*/
 };
 
 window.searchYouTube = searchYouTube;
